0b0f6d65969dfbf0642518071483c5cffef76296
Fabian Tollenaar <fabian@decipher.industries>
2019-05-28 17:27:29 +0200

    Build port version

---
+++ build/precache-manifest.620d67b4454dc130a7bab0340196d9f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "00eafea6006460a63e96",
+    "url": "/static/js/main.ed984ce4.chunk.js"
+  },
+  {
+    "revision": "ccb9f24f428aa4e1368c",
+    "url": "/static/js/2.9cb4a896.chunk.js"
+  },
+  {
+    "revision": "00eafea6006460a63e96",
+    "url": "/static/css/main.46189b57.chunk.css"
+  },
+  {
+    "revision": "6862ab96ab097ae9bf9e3adb8b9105bc",
+    "url": "/index.html"
+  }
+];
